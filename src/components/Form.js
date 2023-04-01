1b0887ceeea1926aa1e8f950d1e9283665400c95
Heeji Hyun <wlgmlgus@gmail.com>
2023-04-01 06:30:00 -0400

    completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,68 @@
-import React, { useState } from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+// src/components/Form
+import React from "react";
 
+function Form({
+  firstName,
+  lastName,
+  handleFirstNameChange,
+  handleLastNameChange,
+}) {
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
 }
 
 export default Form;
+
+
+// <==============================================================================>
+
+
+// import React, { useState } from "react";
+
+// function Form() {
+//   const [firstName, setFirstName] = useState("John");
+//   const [lastName, setLastName] = useState("Henry");
+//   const [newsletter, setNewsletter] = useState(false);
+
+// function handleFirstNameChange(e) {
+//   setFirstName(e.target.value)
+// }
+
+// function handleLastNameChange(e) {
+//   setLastName(e.target.value)
+// }
+
+// function handleNewsletterChange(e) {
+//   // .checked, not .value!
+//   setNewsletter(e.target.checked);
+// }
+
+//   return (
+//     <>
+//       <form>
+//         <input type="text" onChange={handleFirstNameChange} value={firstName} />
+//         <input type="text" onChange={handleLastNameChange} value={lastName} />
+//         <button type="submit">Submit</button>
+//       </form>
+//       <form>
+//         <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+//         <input
+//           type="checkbox"
+//           id="newsletter"
+//           onChange={handleNewsletterChange}
+
+//           checked={newsletter}
+//         />
+//         <button type="submit">Submit</button>
+//       </form>
+//     </>
+//   );
+// }
+
+// export default Form;
